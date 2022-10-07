4542d20f75c81e9f9a6258f879d1ff348f7d93a4
Gerhard Ahrens <gerhard.ahrens@pta.de>
2022-10-07 14:50:46 +0200

    Add Klassenbeschreibung

--- DifferenceTwoListLib/Core/DifferenceFromCollection.cs
+++ DifferenceTwoListLib/Core/DifferenceFromCollection.cs
@@ -1,3 +1,27 @@
+/*
+ * <copyright file="DifferenceCollection.cs" company="Lifeprojects.de">
+ *     Class: DifferenceCollection
+ *     Copyright © Lifeprojects.de 2022
+ * </copyright>
+ *
+ * <author>Gerhard Ahrens - Lifeprojects.de</author>
+ * <email>gerhard.ahrens@lifeprojects.de</email>
+ * <date>07.10.2022</date>
+ * <Project>DifferenceTwoListLib</Project>
+ *
+ * <summary>
+ * Die Klasse ermittelt zwischen 2 Collection die Unterschiede und gibt diese als Liste zurück.
+ * </summary>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the Free Software Foundation, 
+ * either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.You should have received a copy of the GNU General Public License along with this program. 
+ * If not, see <http://www.gnu.org/licenses/>.
+*/
+
 namespace DifferenceTwoListLib
 {
     using System;
