218f7acbb1c653d4d362a1809119d851c5494c5a
GerhardAhrens <gerhard.ahrens@lifeprojects.de>
2022-10-27 19:13:07 +0200

    Document Header für Klassen aktualisiert

--- DifferenceTwoListLib/Core/DifferenceResultItem.cs
+++ DifferenceTwoListLib/Core/DifferenceResultItem.cs
@@ -1,4 +1,28 @@
-﻿namespace DifferenceTwoListLib
+﻿/*
+ * <copyright file="DifferenceResultItem.cs" company="Lifeprojects.de">
+ *     Class: DifferenceResultItem
+ *     Copyright © Lifeprojects.de 2022
+ * </copyright>
+ *
+ * <author>Gerhard Ahrens - Lifeprojects.de</author>
+ * <email>developer@lifeprojects.de</email>
+ * <date>05.10.2022</date>
+ * <Project>DifferenceTwoListLib</Project>
+ *
+ * <summary>
+ * Klasse für
+ * </summary>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the Free Software Foundation, 
+ * either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.You should have received a copy of the GNU General Public License along with this program. 
+ * If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace DifferenceTwoListLib
 {
     using System;
     using System.Diagnostics;
--- DifferenceTwoList_Test/DifferenceCollection_Test.cs
+++ DifferenceTwoList_Test/DifferenceCollection_Test.cs
@@ -1,17 +1,26 @@
-//-----------------------------------------------------------------------
-// <copyright file="UnitTestClass1.cs" company="www.pta.de">
-//     Class: UnitTestClass1
-//     Copyright © www.pta.de 2022
-// </copyright>
-//
-// <author>Gerhard Ahrens - www.pta.de</author>
-// <email>gerhard.ahrens@pta.de</email>
-// <date>05.10.2022 14:43:48</date>
-//
-// <summary>
-// Klasse für 
-// </summary>
-//-----------------------------------------------------------------------
+/*
+ * <copyright file="DifferenceCollection_Test.cs" company="Lifeprojects.de">
+ *     Class: DifferenceCollection_Test
+ *     Copyright © Lifeprojects.de 2022
+ * </copyright>
+ *
+ * <author>Gerhard Ahrens - Lifeprojects.de</author>
+ * <email>developer@lifeprojects.de</email>
+ * <date>05.10.2022</date>
+ * <Project>DifferenceTwoListLib</Project>
+ *
+ * <summary>
+ * Abstrakte Klasse zur Erstellung einer ViewModel Klasse 
+ * </summary>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the Free Software Foundation, 
+ * either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.You should have received a copy of the GNU General Public License along with this program. 
+ * If not, see <http://www.gnu.org/licenses/>.
+*/
 
 namespace DifferenceTwoList_Test
 {
